5bd8187f0e0730fd40679081b8ea4f64f464527d
KERRCAM <kerrcameron@hotmail.com>
2023-10-12 16:33:37 +0100

    new funcs

--- genFuncs.hs
+++ genFuncs.hs
@@ -42,3 +42,24 @@ elem' e (x:xs)
     | e == x = True
     | otherwise = elem' e xs
 
+even_sum 0 = 0
+even_sum x = if x `mod` 2 == 0
+    then x + even_sum (x-1)
+    else even_sum (x-1)
+
+evenSum x  -- same function but with gaurds
+    | (x == 0) = 0
+    | (x `mod` 2 == 0) = x + even_sum (x-1)
+    | otherwise = even_sum (x-1)
+
+join xs ys = [ x ++ " " ++ y | x <- xs, y <- ys]
+
+primes n = [x | x <- [1..n], length (factors x) == 2]
+
+factors n = [x | x <- [1..n], n `mod` x == 0]
+
+length' xs = sum [1 | _ <- xs]
+
+removeLowercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
+heads xxs = [ [ x | x <- xs, even x ] | xs <- xxs] --input list of lists get first element from all in a new list
