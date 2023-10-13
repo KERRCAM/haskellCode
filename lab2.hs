27386360746f6f0cb7f44d20d6837a81f0ec7ee5
KERRCAM <kerrcameron@hotmail.com>
2023-10-13 09:31:28 +0100

    q2

--- lab2.hs
+++ lab2.hs
@@ -1,6 +1,16 @@
+--q1
 squareAndCube x = (x*x, x*x*x) 
 addTuple (a, b) = a + b 
 first (a, _) = a 
 second (_, b) = b  
 swap (a, b) = (b, a) 
 twoToThree (a, b) c = (a, b, c) 
+
+--q2 
+headSquared (x:xs) = x*x 
+headSquared2 list = (head list)^2
+third list = list !! 4 
+secondTail list = tail(tail list) 
+thirdHead list = head(tail(tail list)) 
+firstPlusLast list = (head list) + (last list) 
+prependTwo list a b = a : (b : list)
