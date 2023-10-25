a5715610cbf70aaa6a6a2e72869e1d552cb8c91d
KERRCAM <kerrcameron@hotmail.com>
2023-10-25 10:36:39 +0100

    type classes

--- polymorphicTypes.hs
+++ polymorphicTypes.hs
@@ -15,3 +15,47 @@ intTail = tail :: [int] -> [int]
 f (a,b,c,d) = (d,c,b,a) 
 g = f :: (a,a,a,a) -> (a,a,a,a)
 
+{- 
+Exercise
+Which types are returned by the following queries?
+ghci> :t take
+Int -> [a] -> [a]
+
+ghci> :t (:)
+a -> [a] -> [a]
+
+ghci> :t (++)
+[a] -> [a] -> [a]
+-}
+
+--char on front of string uses : 
+--string onto string uses ++
+
+{- 
+Type classes
+ghci> :t (+)
+(+) :: Num a => a -> a -> a
+ghci> :t (*)
+(*) :: Num a => a -> a -> a
+Num is a type class
+- It restricts the type variable a to only be number types
+- Int, Integer, Float, Double are all contained in Num
+- Char, Bool, tuples and lists are not in Num
+:info Num 
+-}
+
+--Eq type class is all types that can be equality tested (==) 
+
+--forces a to be both equality testable and a number
+equalsTwo a b = a + b == 2
+-- :t equalsTwo gives equals_two :: (Eq a, Num a) => a -> a -> Bool
+
+-- Works but too restrictive
+--equalsTwo :: Int -> Int -> Bool
+-- Most general
+--equalsTwo :: (Eq a, Num a) => a -> a -> Bool
+-- Too general (will give error)
+--equalsTwo :: a -> a -> Bool
+
+--obviously all nums are also in eq but still need to state it 
+--this is becasue haskell lets you have num types not in eq
