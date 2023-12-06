3bd7ef123ba928cee890148760610f8c8aefc9e0
KERRCAM <90578826+KERRCAM@users.noreply.github.com>
2023-12-06 17:27:13 +0000

    lab 1 and 3

--- labs/lab2.hs
+++ labs/lab2.hs
@@ -1,26 +1,44 @@
 --q1
 squareAndCube x = (x*x, x*x*x) 
+
 addTuple (a, b) = a + b 
+
 first (a, _) = a 
+
 second (_, b) = b  
+
 swap (a, b) = (b, a) 
+
 twoToThree (a, b) c = (a, b, c) 
+
 
 --q2 
 headSquared (x:xs) = x*x 
+
 headSquared2 list = (head list)^2
+
 third list = list !! 4 
+
 secondTail list = tail(tail list) 
+
 thirdHead list = head(tail(tail list)) 
+
 firstPlusLast list = (head list) + (last list) 
+
 prependTwo list a b = a : (b : list) 
+
 
 --q3 
 twoLengths list1 list2 = (length list1) + (length list2) 
+
 makePalindrome list = list ++ (reverse list) 
+
 sumAndProduct list = ((sum list),(product list)) 
+
 fourThroughSix list = take 3 (drop 3 list) 
+
 bothIn list x y = ((elem x list) && (elem y list)) 
+
 
 {-q4
 a) [101..200] 
@@ -29,12 +47,18 @@ c) [20, 19..1]
 d) [999, 1002..] --control c to stop 
 -}
 
+
 --q5 
 --a) [2^x | x <- [1..10]] 
+
 onlyOdds list = [x | x <- list, mod x 2 == 1] 
+
 between a b list = [x | x <- list, (x > a) && (x < b)] --a must be < b 
+
 numOfEs list = length [x | x <- list, (x == 'e') || (x == 'E')] 
+
 --fizzbuzz = [if ((if mod x 3 then "fizz" else show x) && (if mod x 3 then "fizz" else show x)) then "fizzbuzz" else show x | x <- [1..]] bad attempt 
+
 --solution 
 proper_fizzbuzz = [
     let div_3 = x `mod` 3 == 0
--- labs/lab3.hs
+++ labs/lab3.hs
@@ -29,4 +29,67 @@ triple [] = []
 triple (x:xs) = x : x : x : triple xs 
 
 multiAdjacent [] = [] 
+multiAdjacent [_] = error "called on list with odd length"
 multiAdjacent (x:y:xs) = (x * y) : multiAdjacent xs
+
+getEle i [] = error "list is too short"
+getEle 0 (x:_) = x 
+getEle i (x:xs) = getEle (i - 1) xs
+
+dropEle i [] = error "list is too short"
+dropEle 0 (_:xs) = xs
+dropEle i (x:xs) = x : dropEle (i - 1) xs
+
+divList [] [] = [] 
+divList [] _ = error "lists are different length"
+divList _ [] = error "lists are different length"
+divList (x:xs) (y:ys) = x / y : divList xs ys
+
+longer [] _ = False 
+longer [] [] = False 
+longer _ [] = True 
+longer (_:xs) (_:ys) = longer xs ys
+
+div3Anot [] = ([], [])
+div3Anot (x:xs)
+    | mod x 3 == 0 = (x:first, second)
+    | otherwise = (first, x:second)
+    where (first, second) = div3Anot xs
+
+vowlesAcons [] = ([], [])
+vowlesAcons (x:xs) 
+    | elem x "aeiou" = (x:vow, cons)
+    | otherwise = (vow, x:cons)
+    where (vow, cons) = vowlesAcons xs
+
+fastLucasHelp 1 = [1,2]
+fastLucasHelp n = x + y : (x:y:rest)
+    where (x:y:rest) = fastLucasHelp (n - 1)
+
+fastLucas n = head (fastLucasHelp n)
+
+multPairs [] = [] 
+multPairs ((x, y):xs) = x * y : multPairs xs
+
+multByPos list = multPairs (zip list [0..])
+
+
+
+-- Returns the length of the collatz sequence starting at n
+collatz 1 = 1
+collatz n
+    | n `mod` 2 == 0 = 1 + collatz (div n 2)
+    | otherwise = 1 + collatz (3*n + 1)
+-- Returns (starting_number, sequence_length) for the longest sequence less than n
+longest_collatz_helper 1 = (1,1)
+longest_collatz_helper n =
+    let
+        (start, length) = longest_collatz_helper (n-1)
+        our_length = collatz n
+    in
+        if our_length > length
+        then (n, our_length)
+        else (start, length)
+longest_collatz n = start
+    where (start, _) = longest_collatz_helper n
+
